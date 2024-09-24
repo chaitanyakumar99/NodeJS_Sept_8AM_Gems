171531169b54bc7db6c772b6427d493e540ef994
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-09-24 09:24:14 +0530

    TS

--- CRUDJSON/routes/apiRouter.js
+++ CRUDJSON/routes/apiRouter.js
@@ -46,8 +46,27 @@ Access Type:Public
 Note: we need to verify employee exist or not. 
 If employee exit we are going to update employee
 */
-router.put("/update/:id",(req,resp)=>{
-    let eid = req.params.id
+router.put("/update/:id", async(req,resp)=>{
+    let emp_Id = req.params.id;
+    let employee = req.body;
+    let employees =await getEmployees();
+    console.log(emp_Id)
+    console.log(employee)
+    console.log(employees)
+    let emp_Data=employees.find((emp)=>{
+        return emp.eid ==emp_Id
+    })
+    console.log(emp_Data)
+    if(!emp_Data){
+        return resp.status(401).json({"msg":"Employee Not Exits"})
+    }
+    let remaining_Employees=employees.filter((emp)=>{
+        return emp.eid != emp_Id
+    })
+    remaining_Employees.unshift(employee);
+    console.log(remaining_Employees)
+    savemployees(remaining_Employees);
+    return resp.status(200).json({"msg":"Employee updated successfully!"})
 });
 
 /*
@@ -59,8 +78,21 @@ Access Type:Public
 Note: we need to verify employee exist or not. 
 If employee exits we are going to delete employee
 */
-router.delete("/del/:id",(req,resp)=>{
-    let eid= req.params.id
+router.delete("/del/:id", async (req,resp)=>{
+    let emp_Id= req.params.id;
+    let employees= await getEmployees();
+    let emp_Data = employees.find((emp)=>{
+        return emp.eid  == emp_Id
+    })
+    if(!emp_Data){
+        return resp.status(401).json({"msg":"Employee Does Not Exits"})
+    }
+    let remaining_Employees = employees.filter((emp)=>{
+        return emp.eid !=emp_Id;
+    })
+    savemployees(remaining_Employees);
+    return resp.status(200).json({"msg":"Deleted Successfully"})
+
 })
 
 
